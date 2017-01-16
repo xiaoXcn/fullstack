69be3a3afc5e25aa3b743415183a9dc231769ea6
xiaoXcn <mingming1014@yeah.net>
2017-01-16 17:07:58 +0800

    practwo

--- feprac/src/main/webapp/WEB-INF/scripts/practwo.js
+++ feprac/src/main/webapp/WEB-INF/scripts/practwo.js
@@ -4,6 +4,11 @@ function changeBackgroundColor(){
 	if(!document.getElementById("color-list")) return false;
 	var liEles = document.getElementById("color-list").getElementsByTagName("li");
 	for(var i=0;i<liEles.length;i++){
-		
+		liEles[i].onclick = function(){
+			var bodyEle = document.getElementsByTagName("body")[0];
+			addClass(bodyEle,this.className);
+		}
 	}
 }
+
+addToOnload(changeBackgroundColor);
